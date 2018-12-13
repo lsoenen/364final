4d84d90acd0ba86da2945dcb3d6d7b650b9597fd
Luke Soenen <lukesoenen@gmail.com>
2018-12-12 20:12:40 -0500

    users

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('index'))
+            return redirect(request.args.get('next') or url_for('home'))
         flash('Invalid username or password.')
     return render_template('base.html',form=form)
 
